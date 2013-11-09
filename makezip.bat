49cc8a44f83e68fa147c269835d22eb5d4024ff1
Hohol <nikita.glashenko@gmail.com>
2013-11-09 17:57:33 +0400

    Zip script added

---
+++ makezip.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Hohol\Desktop\CodeTroopers\java-cgdk\src\main\java
+zip -@ C:\Users\Hohol\Desktop\CodeTroopers\java-cgdk\HoholTroopers.zip < C:\Users\Hohol\Desktop\CodeTroopers\java-cgdk\files_to_submit.txt
+pause
