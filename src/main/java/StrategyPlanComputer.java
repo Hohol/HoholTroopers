5d1672299f7ed5592e718d7ea1af8de587cfcb99
Hohol <nikita.glashenko@gmail.com>
2013-12-13 19:33:54 +0400

    Changed constant in danger area definition for 3-units fights

--- src/main/java/StrategyPlanComputer.java
+++ src/main/java/StrategyPlanComputer.java
@@ -59,7 +59,7 @@ public class StrategyPlanComputer extends AbstractPlanComputer<StrategyState> {
                         if (isWall(shooterX, shooterY)) {
                             continue;
                         }
-                        int shootRange = 10;
+                        int shootRange = initialTeamSize >= 4 ? 10 : 8;
                         if (!reachable(shooterX, shooterY, targetX, targetY, Math.min(shooterStance, targetStance), shootRange)) {
                             continue;
                         }
